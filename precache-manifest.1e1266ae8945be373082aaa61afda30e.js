46671ce3ad2446cae1000c78da1c2b971d83023e
harshwardhan <iharshwardhanchaudhary@gmail.com>
2020-09-09 23:14:32 +0530

    Updates

---
+++ precache-manifest.1e1266ae8945be373082aaa61afda30e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "449a1ff4996024c1429ed53423d46a4e",
+    "url": "/react-recipeapp/index.html"
+  },
+  {
+    "revision": "8f33c736587559c0e372",
+    "url": "/react-recipeapp/static/css/main.a06deb60.chunk.css"
+  },
+  {
+    "revision": "de3bf90b0438b417337a",
+    "url": "/react-recipeapp/static/js/2.cd117ecc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-recipeapp/static/js/2.cd117ecc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f33c736587559c0e372",
+    "url": "/react-recipeapp/static/js/main.e3aa252b.chunk.js"
+  },
+  {
+    "revision": "19c3bb956dd19efa994f",
+    "url": "/react-recipeapp/static/js/runtime-main.acc57f14.js"
+  }
+]);
